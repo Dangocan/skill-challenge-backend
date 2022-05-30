841a0d73d3aa2717ce5349de05b9f5715a1cdcd6
Dangocan <danilogc1999@gmail.com>
2022-05-30 17:37:18 -0300

    Fix async calls

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -19,27 +19,30 @@ export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
 
   @Post()
-  create(@Body() createTicketDto: CreateTicketDto) {
-    return this.ticketService.create(createTicketDto);
+  async create(@Body() createTicketDto: CreateTicketDto) {
+    return await this.ticketService.create(createTicketDto);
   }
 
   @Get()
-  findAll() {
-    return this.ticketService.findAll();
+  async findAll() {
+    return await this.ticketService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ticketService.findOne({ id });
+  async findOne(@Param('id') id: string) {
+    return await this.ticketService.findOne({ id });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTicketDto: UpdateTicketDto) {
-    return this.ticketService.update(id, updateTicketDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateTicketDto: UpdateTicketDto,
+  ) {
+    return await this.ticketService.update(id, updateTicketDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ticketService.remove(id);
+  async remove(@Param('id') id: string) {
+    return await this.ticketService.remove(id);
   }
 }
