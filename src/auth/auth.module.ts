acf76422aadbacee4b09ab3c6c51ae2fbb5a8917
Dangocan <danilogc1999@gmail.com>
2022-06-01 19:40:22 -0300

    Modify token expiration time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule,
     JwtModule.register({
       privateKey: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '30m' },
+      signOptions: { expiresIn: '8h' },
     }),
   ],
   controllers: [AuthController],
